1aa1d9dec022b78484f94d1b034105d8199b11ea
CJC9 <engnatruve@gmail.com>
2016-11-30 21:22:41 -0500

    Vistas y controladores de user y producto

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,42 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    find_product
+    if @product.nil?
+      flash[:error] = "No existe el producto"
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+      if @product.save
+        redirect_to products_path
+      end
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+  private
+
+  def find_product
+    @product = Product.find_by(id: params[:id])
+  end
+
+  #strong parameters
+  def product_params
+    params.require(:product).permit(:id_laboratorio , :nombre , :precio_compra , :imagen)
+  end
+end
